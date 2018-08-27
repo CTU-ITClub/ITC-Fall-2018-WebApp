f9821b78733e96c041b8699e217e6d370081dc9d
nguyenduongy <39272571+nguyenduongy@users.noreply.github.com>
2018-08-27 20:59:49 +0700

    them ham nickname

--- application/controllers/Hello.php
+++ application/controllers/Hello.php
@@ -1,1 +1,14 @@
 <?php
+if (!defined('BASEPATH'))
+exit('No direct script access allowed');
+
+class Hello extends CI_Controller {
+
+    public function index() {
+        echo 'Freetuts.net Hello Controller';
+    }
+
+    public function nickname($user='Nhung',$callback='lun') {
+        echo 'Nickname cua '.$user.' la '.$user.' '.$callback;
+    }
+}
